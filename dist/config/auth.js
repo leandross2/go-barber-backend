0746267efeacac3ba0ae7e4764c9a096d55e49ea
Leandro Saraiva <leandross91@gmail.com>
2020-06-08 22:45:45 -0300

    build

---
+++ dist/config/auth.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    jwt: {
+        secret: 'c12b55425150525d955e10e0007325f8',
+        expiresIn: '7d',
+    },
+};
