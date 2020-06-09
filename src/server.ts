ea5723ce7bba0ade617e8278734594a09d11aba6
Leandro Saraiva <leandross91@gmail.com>
2020-06-08 23:12:16 -0300

    rota sucesso

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
+app.get('/', (request: Request, response: Response) => {
+  response.json({ sucesso: true });
+});
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
